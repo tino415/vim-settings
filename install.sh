f4116c639ac7ffe959df936c2baa6e5c3ef3dbb1
tino415 <cernakmartin3@gmail.com>
2014-11-24 09:55:57 +0100

    Updated install file to automaticaly clone some plugins from github

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ln -s $PROJECT_DIR/vimrc ~/.vimrc
 ln -s $PROJECT_DIR/vim ~/.vim
+
+git clone https://github.com/godlygeek/tabular.git vim/bundle/tabular
+git clone https://github.com/terryma/vim-multiple-cursors.git vim/bundle/vim-multiple-cursors
+git clone https://github.com/scrooloose/syntastic.git vim/bundle/syntastic
